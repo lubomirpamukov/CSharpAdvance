56532cf37ef306ba2f11d7deda58de5421608680
lubomirpamukov <lubomir.pamukov@gmail.com>
2023-08-26 22:00:45 +0300

    First exercise

---
+++ 01. Reverse a string/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01._Reverse_a_string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToArray();
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                stack.Push(c);
+            }
+
+            while (stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
